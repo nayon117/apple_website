99ea45a5af15db75601b791272c363b48eb7ff76
nayon117 <hasibul.nayon1@gmail.com>
2024-06-30 11:55:40 +0600

    implement animated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,65 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Hero = () => {
-    return (
-        <section className=''>
-                Welcome to Hero
-        </section>
-    )
-}
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 768 ? smallHeroVideo : heroVideo
+  );
+
+  // Change video source based on screen size
+  const handleVideoSrcSet = () => {
+    setVideoSrc(window.innerWidth < 768 ? smallHeroVideo : heroVideo);
+  };
+
+  // Event listener for screen resize
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  // GSAP animations
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+  }, []);
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 pro
+        </p>
+
+        {/* video part */}
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      {/* CTA */}
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">from $199 month or $999</p>
+      </div>
+    </section>
+  );
+};
 export default Hero;
